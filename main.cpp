105fcfede5eb4a656b5b5bd8a4d93c6771cc30c4
sararaa <84247798+sararaa@users.noreply.github.com>
2022-09-30 23:39:14 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,23 @@
+// abs complex example
+#include <iostream>     // std::cout
+#include <complex>      // std::complex, std::abs
+using namespace std;
 
+int main ()
+{
+    double re = -0.77568377;
+    double im = 0.13646737;
+    complex<double> c (re, im);
+    complex<double> z (0,0);
+    cout << "c = " << c << "\t";
+    cout << "|c| = " << abs(c) << endl;
+    cout << "Real component of c: " << c.real() << endl;
+    cout << "Imaginary component of c: " << c.imag() << endl;
+    z = z*z + c;
+    cout << "z_2 = " << z << "\t";
+    cout << "|z_2| = " << abs(z) << endl;
+    z = z*z + c;
+    cout << "z_3 = " << z << "\t";
+    cout << "|z_3| = " << abs(z) << endl;
+    return 0;
+}
