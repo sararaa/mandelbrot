3fc0f27a592ca9015687d8e0a8b35b9b67acad15
sararaa <84247798+sararaa@users.noreply.github.com>
2022-10-03 13:18:58 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,161 +1,141 @@
 #include "ComplexPlane.h"
-
+ 
 int main()
 {
-
-	//Calculate Aspect Ratio
-	double screenwidth = VideoMode::getDesktopMode().width;
-	double screenheight = VideoMode::getDesktopMode().height;
-	double aspectRatio = screenwidth/screenheight;
-
-	// Create a video mode object
-	VideoMode vm(screenwidth, screenwidth);
-
-
-	// Create and open a window for the game
-    //create not boring name 
-	RenderWindow window(vm, "Mandelbrot", Style::Default);
-
-	//Declare ComplexPlane object
-	ComplexPlane complex_p(aspectRatio);
-
-	//Declare Font and Text objects, Set font of messageText
-	Text messageText;
-    Font font;
-    //CHANGE THE FONT 
-    font.loadFromFile("fonts/calibri.ttf");
-    messageText.setFont(font);
-
-	//Declare Vertex Array, Set primitive type points, Set to size of screen
-    // DOUBLE CHECK VARIABLES
-	VertexArray vArray;
-	vArray.setPrimitiveType(Points);
-	vArray.resize(screenwidth*screenheight);
-
-	// Set Enum class to Calculating
-	windowStatus status= windowStatus::CALCULATING;
-
-	//Event declaration
-	Event event;
-
-
-	while (window.isOpen())
-	{
-
-		/*
-		****************************************
-		Handle the players input
-		****************************************
-		*/
-
-		while (window.pollEvent(event)){
-
-			if (event.type == Event::Closed)
-			{
-   				 window.close();
-			}
-
-			if (event.type == Event::MouseButtonPressed)
-			{
-				//Find mouse Clicked coordinate
-				Vector2i mouse_pos;
-				Vector2f mouse_Coord;
-
-				mouse_pos = window.mapCoordsToPixel(Vector2f(event.mouseButton.x, event.mouseButton.y), complex_p.getView());
-
-				mouse_Coord= Vector2f(mouse_pos);
-
-
-				// Right click zooms out
-				if (event.mouseButton.button == Mouse::Right)
-    			{
-					complex_p.zoomOut();
-					complex_p.setCenter(mouse_Coord);
-
-
-				}
-				//Left click zooms in
-				if (event.mouseButton.button == Mouse::Left)
-    			{
-					complex_p.zoomIn();
-					complex_p.setCenter(mouse_Coord);
-
-				}
-
-				status= windowStatus::CALCULATING;
-
-			}
-
-			if(event.type == Event::MouseMoved)
-			{
-				//Find Cursor Coordinate
-				Vector2i mouse_pos;
-				Vector2f mouse_Coord;
-
-				mouse_pos = window.mapCoordsToPixel(Vector2f(event.mouseMove.x, event.mouseMove.y), complex_p.getView());
-				mouse_Coord= Vector2f(mouse_pos);
-
-				complex_p.setMouseLocation(mouse_Coord);
-			}
-
-		}
-
-		if (Keyboard::isKeyPressed(Keyboard::Escape))
-		{
-			window.close();
-		}
-
-		/*
-		****************************************
-		Update the scene
-		****************************************
-		*/
-// EDIT THIS PORTION
-		if (status == windowStatus::CALCULATING)
-		{
-
-			for (int i=0; i< screenwidth; i++)
-			{
-			for (int j=0;j<screenheight; j++)
-				{
-					Uint8 r, g, b;
-					Vector2i pixelPos;
-					Vector2f pixelCoord;
-
-					vArray[j+i*screenwidth].position={(float)i,(float)j};
-					pixelPos = window.mapCoordsToPixel(Vector2f(i, j), complex_p.getView());
-					pixelCoord = Vector2f(pixelPos);
-
-					size_t count= complex_p.countIterations(pixelCoord);
-
-					complex_p.iterationsToRGB(count,r,g,b);
-
-					vArray[j+i*screenwidth].color={r,g,b};
-
-
-				}
-			}
-
-			status = windowStatus::DISPLAYING;
-			complex_p.loadText(messageText);
-		}
-
-
-		// Clear everything from the last frame
-		window.clear();
-
-		//Draw vertex array
-
-		window.draw(vArray);
-
-		// Draw text
-		window.draw(messageText);
-		
-		// Show everything we just drew
-		window.display();
-
-
-	}
-
-	return 0;
+ 
+   //aspect ratio stuff
+   double width = VideoMode::getDesktopMode().width;
+   double height = VideoMode::getDesktopMode().height;
+   double aspectRatio = width/height;
+ 
+   //vid mode obj :0
+   VideoMode vm(width, width);
+ 
+ 
+   // open a windowww
+   RenderWindow window(vm, "Mandelbrot Project by Taylor and Senara!", Style::Default);
+ 
+   //complexplane obj
+   ComplexPlane complex(aspectRatio);
+ 
+   //text + font obj
+   Text messageText;
+   Font font;
+ 
+ 
+   //super cool and never been used before font
+   font.loadFromFile("./font/Game Of Squids.ttf");
+   messageText.setFont(font);
+ 
+   //array declared 
+   VertexArray array;
+   array.setPrimitiveType(Points);
+   array.resize(width * height);
+ 
+   // set enum status to calc
+   windowStatus status= windowStatus::CALCULATING;
+ 
+   //event obj declared
+   Event event;
+ 
+ 
+   while (window.isOpen())
+   {
+ 
+       while (window.pollEvent(event)){
+ 
+           if (event.type == Event::Closed)
+           {
+                window.close();
+           }
+ 
+           if (event.type == Event::MouseButtonPressed)
+           {
+               //coords
+               Vector2f coords;
+ 
+               coords = window.mapPixelToCoords(Vector2i(event.mouseButton.x, event.mouseButton.y), complex.getView());
+ 
+ 
+               // right click --> zoom out
+               if (event.mouseButton.button == Mouse::Right)
+               {
+                   complex.zoomOut();
+                   complex.setCenter(coords);
+ 
+ 
+               }
+               //left click --> zoom in
+               if (event.mouseButton.button == Mouse::Left)
+               {
+                   complex.zoomIn();
+                   complex.setCenter(coords);
+ 
+               }
+ 
+               status= windowStatus::CALCULATING;
+ 
+           }
+ 
+           if(event.type == Event::MouseMoved)
+           {
+               //Find Cursor Coordinate
+               Vector2f coords;
+ 
+               coords = window.mapPixelToCoords(Vector2i(event.mouseMove.x, event.mouseMove.y), complex.getView());
+ 
+               complex.setMouseLocation(coords);
+           }
+ 
+       }
+ 
+       if (Keyboard::isKeyPressed(Keyboard::Escape))
+       {
+           window.close();
+       }
+   if (status == windowStatus::CALCULATING)
+       {
+   for (int i=0; i < width; i++)
+           {
+           for (int j=0; j < height; j++)
+               {
+                   Uint8 r, g, b = 255;
+                   size_t count = 0;
+                   Vector2i points{j,i};
+                   Vector2f coords;
+ 
+                   array[j+i*width].position={(float)i,(float)j};
+                   points = window.mapCoordsToPixel(Vector2f(i, j), complex.getView());
+                   coords = Vector2f(points);
+ 
+                   count= complex.countIterations(coords);
+ 
+                   complex.iterationsToRGB(count,r,g,b);
+ 
+                   array[j+i*width].color={r,g,b};
+ 
+ 
+               }
+           }
+ 
+           status = windowStatus::DISPLAYING;
+           complex.loadText(messageText);
+       }
+ 
+ 
+       window.clear();
+ 
+       window.draw(array);
+ 
+       window.draw(messageText);
+      
+       window.display();
+ 
+ 
+   }
+ 
+   return 0;
 }
+ 
+ 
+
